e86d73e40f206dcd2827883c6f17da2a2fab9583
Ivan Necas <necasik@gmail.com>
2011-03-01 22:43:08 +0100

    Move documented class from spec to separeted file

--- spec/active_doc/methods_doc_spec.rb
+++ spec/active_doc/methods_doc_spec.rb
@@ -1,31 +1,8 @@
 require 'spec_helper'
+require File.expand_path("../../support/documented_class", __FILE__)
 
 describe ActiveDoc::MethodsDoc do
   describe "arguments validation" do
-    class ClassWithMethodValidation
-      include ActiveDoc
-
-      takes :first_name, String
-      takes :last_name, String
-
-      def say_hello_to(first_name, last_name)
-        return "Hello #{first_name} #{last_name}"
-      end
-
-      takes :message, String
-
-      def self.announce(message)
-        return "People of the Earth, hear the message: '#{message}'"
-      end
-
-      def self.announce_anything(sound)
-        return "People of the Earth, hear the sound: '#{sound}'"
-      end
-
-      def say_hello_to_any_name(name)
-        return "Hello #{name}"
-      end
-    end
     subject { ClassWithMethodValidation.new }
     context "instance method" do
       context "with wrong type" do
