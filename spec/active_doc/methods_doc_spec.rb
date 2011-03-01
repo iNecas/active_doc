6901881a570bde97533c2c787592c20941662ab8
Ivan Necas <necasik@gmail.com>
2011-03-01 21:19:47 +0100

    white space

--- spec/active_doc/methods_doc_spec.rb
+++ spec/active_doc/methods_doc_spec.rb
@@ -7,15 +7,17 @@ describe ActiveDoc::MethodsDoc do
 
       describe_arg :first_name, String
       describe_arg :last_name, String
+
       def say_hello_to(first_name, last_name)
         return "Hello #{first_name} #{last_name}"
       end
-      
+
       describe_arg :message, String
+
       def self.announce(message)
         return "People of the Earth, hear the message: '#{message}'"
       end
-      
+
       def self.announce_anything(sound)
         return "People of the Earth, hear the sound: '#{sound}'"
       end
@@ -26,23 +28,23 @@ describe ActiveDoc::MethodsDoc do
     end
     subject { ClassWithMethodValidation.new }
     context "instance method" do
-    context "with wrong type" do
-      it "raises ArgumentError" do
-        lambda { subject.say_hello_to(0, "Necas") }.should raise_error ArgumentError
+      context "with wrong type" do
+        it "raises ArgumentError" do
+          lambda { subject.say_hello_to(0, "Necas") }.should raise_error ArgumentError
+        end
       end
-    end
 
-    context "with correct type" do
-      it "does not raise ArgumentError" do
-        lambda { subject.say_hello_to("Ivan", "Necas") }.should_not raise_error ArgumentError
+      context "with correct type" do
+        it "does not raise ArgumentError" do
+          lambda { subject.say_hello_to("Ivan", "Necas") }.should_not raise_error ArgumentError
+        end
       end
-    end
 
-    context "without argument validation" do
-      it "does not raise ArgumentError" do
-        lambda { subject.say_hello_to_any_name(0) }.should_not raise_error ArgumentError
+      context "without argument validation" do
+        it "does not raise ArgumentError" do
+          lambda { subject.say_hello_to_any_name(0) }.should_not raise_error ArgumentError
+        end
       end
-    end
     end
 
     context "class method" do
