80033d2851c762c23abd7315856608449f6f04c4
Ivan Necas <necasik@gmail.com>
2011-03-03 18:11:08 +0100

    Don't use the same test subject in different specs
    It makes it too coupled. When need to change in one spec, it forces you tu update another.

--- spec/active_doc/methods_doc_spec.rb
+++ spec/active_doc/methods_doc_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
-require File.expand_path("../../support/documented_class", __FILE__)
+class ClassWithMethodValidation
+  include ActiveDoc
+
+  takes :first_name, String
+  takes :last_name, String
+
+  def say_hello_to(first_name, last_name)
+    return "Hello #{first_name} #{last_name}"
+  end
+
+  takes :message, String
+
+  def self.announce(message)
+    return "People of the Earth, hear the message: '#{message}'"
+  end
+
+  def self.announce_anything(sound)
+    return "People of the Earth, hear the sound: '#{sound}'"
+  end
+
+  def say_hello_to_any_name(name)
+    return "Hello #{name}"
+  end
+end
 
 describe ActiveDoc::MethodsDoc do
   describe "arguments validation" do
