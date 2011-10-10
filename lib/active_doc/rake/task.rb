5678f75cd12f8ce3e7a81cdfbd7853abeca08809
Ivan Necas <necasik@gmail.com>
2011-10-10 23:39:14 +0200

    White space

--- lib/active_doc/rake/task.rb
+++ lib/active_doc/rake/task.rb
@@ -18,18 +18,18 @@ module ActiveDoc
     # This will define a task named <tt>active_doc</tt> described as 'Generate ActiveDoc RDoc documentation'. 
     class Task
       class GenerateRDocRunner #:nodoc:
-        
+
         def initialize(source_dir, output_dir)
           @source_dir, @output_dir = source_dir, output_dir
         end
-        
+
         def run
           ActiveDoc::RdocGenerator.write_rdoc_for_dir(@source_dir, @output_dir) do |file, documented_methods|
             STDOUT.puts "Generating RDoc for #{file}..."
           end
         end
       end
-      
+
       def initialize(source_dir, output_dir, task_name = "active_doc", desc = "Generate ActiveDoc RDoc documentation")
         @source_dir, @output_dir = source_dir, output_dir
         @task_name, @desc = task_name, desc
